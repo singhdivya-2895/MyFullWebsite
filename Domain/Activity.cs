d8500ff8d85930e39435d2ca17ee09ac193f8886
Divya Singh <singhdivya.2895@gmail.com>
2023-06-06 13:11:57 +0000

    Initial Checkin

---
+++ Domain/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Activity
+    {
+        [Key]
+        // This Annotation is required when you using custom attribute as your key.
+        // For Example: MyId
+        public Guid  Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+    }
+}
