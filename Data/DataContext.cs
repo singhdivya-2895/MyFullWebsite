33dfade22eef4d2a7a208d9048197f0e0fed2f25
singhdivya-2895 <129833279+singhdivya-2895@users.noreply.github.com>
2023-06-06 13:13:02 +0000

    Initial Checkin (#1)

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
