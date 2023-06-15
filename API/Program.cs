b1beb27fb91a9bea12f8b9b8882ab6b735892b57
singhdivya-2895 <129833279+singhdivya-2895@users.noreply.github.com>
2023-06-15 12:55:57 +0000

    Frontend with Web Api Connectivity (#3)
    * React Setup
    
    * CORS issue and Semanti AI

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration
-        .GetConnectionString("DefaultConnection"));
+       .GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
 });
 
 var app = builder.Build();
@@ -24,6 +31,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
